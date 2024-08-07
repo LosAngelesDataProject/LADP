085f997cd29184a59d47ad18e9a83da240320b75
Jesse James Canell Jr <119881707+jjcanell77@users.noreply.github.com>
2024-08-07 14:03:49 -0500

    Added EFC API and moved react into its own folder

---
+++ dotnet/LADP_EFC/Models/BusinessHours.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LADP__EFC.Models;
+
+public class BusinessHours
+{
+    public int BusinessHourId { get; set; }
+    public int FoodResourceId { get; set; } // Required foreign key property
+    public int DayId { get; set; } // Required foreign key property
+    public Day Day { get; set; } = null!; // Many-to-One relationship with Days
+    public string? OpenTime { get; set; }
+    public string? CloseTime { get; set; }
+
+    public FoodResource FoodResource { get; set; } = null!; // Many-to-One relationship with FoodResource
+}
---
+++ react/src/components/navBar/NavBar.jsx
@@ -0,0 +1,69 @@
+import { Link, useLocation } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import PropTypes from "prop-types";
+import logo from "../../assets/images/logo.png";
+import styles from "./NavBar.module.css";
+
+const NavBar = (props) => {
+  const { isLoggedIn, onLogout } = props;
+
+  const location = useLocation(); // Get the current location
+  const isActive = (path) => location.pathname === path ? styles.active : '';
+
+  return (
+    <Navbar className={styles.background} expand="lg">
+      <Container>
+        <Nav className="me-auto col-md-5 justify-content-start justify-content-around">
+          <Nav.Link className={`${styles.navLink} ${isActive("")}`} as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link className={`${styles.navLink} ${isActive("/calendar")}`} as={Link} to="/calendar">
+            Calendar
+          </Nav.Link>
+          <Nav.Link className={styles.navLink} as={Link} to="/about-us">
+            About Us
+          </Nav.Link>
+        </Nav>
+        <Navbar.Brand
+          as={Link}
+          to="/"
+          className="mx-auto d-flex justify-content-center col-md-2"
+        >
+          <img
+            src={logo}
+            width="140"
+            height="140"
+            alt="logo"
+            className="align-self-center rounded-4"
+          />
+        </Navbar.Brand>
+        <Nav className="ms-auto col-md-5 justify-content-end justify-content-around">
+          <Nav.Link className={styles.navLink} as={Link} to="/contact-us">
+            Contact Us
+          </Nav.Link>
+          {isLoggedIn ? (
+            <Nav.Link className={styles.endText} onClick={onLogout}>
+              Logout
+            </Nav.Link>
+          ) : (
+            <>
+              <Nav.Link className={styles.navLink} as={Link} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link className={styles.endText} as={Link} to="/register">
+                Register
+              </Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+NavBar.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  onLogout: PropTypes.func.isRequired,
+};
+
+export default NavBar;
