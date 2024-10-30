7e6dac601a7d027a00e8c842b6a1dd06566317a6
Jared <117613416+na106@users.noreply.github.com>
2024-10-30 14:39:22 -0700

    Under Construction Page (#49)
    * reduced text size and merge conflicts
    
    * Set Up Construction Page and Url for Register, Login, ContactUs
    
    * construction page renders for login, register, and contactUs
    
    * corrected line spacings in construction.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,13 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AboutUs from "./components/about/AboutUs";
 import Calendar from "./components/calender/Calendar";
+import Construction from "./components/underConstruction/Construction"
 import Footer from "./components/footer/HomeFooter";
 import Home from "./components/home/Home";
-import Login from "./components/login/Login";
+// import Login from "./components/login/Login";
 import Navigate from "./components/Navigate";
-import Register from "./components/register/Register";
+// import Register from "./components/register/Register";
 import NotFound from "./components/NotFound";
-import ContactUs from "./components/contact/ContactUs";
+// import ContactUs from "./components/contact/ContactUs";
 import NavBar from "./components/navBar/NavBar";
 import RegisterOrg from "./components/register/RegisterOrg";
 
@@ -27,12 +28,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/calendar" element={<Calendar />} />
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        {/* <Route path="/login" element={<Login onLogin={handleLogin} />} /> */}
+        <Route path="/login" element={<Construction/>} />
         <Route path="/navigate" element={<Navigate />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Construction/>} />
         <Route path="/register-org" element={<RegisterOrg />} />
-        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/contact-us" element={<Construction/>} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/under-construction" element={<Construction/>} />
       </Routes>
       <Footer />
     </>
