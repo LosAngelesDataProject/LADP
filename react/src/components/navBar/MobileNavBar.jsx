588bc23f4d2b8753ee1867d8b4cb36a03bf68006
Jesse James Canell Jr <119881707+jjcanell77@users.noreply.github.com>
2024-11-06 23:44:59 -0600

    Added react-responsive and basic layout

---
+++ react/src/components/navBar/MobileNavBar.jsx
@@ -0,0 +1,93 @@
+import { Link, useLocation } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import PropTypes from "prop-types";
+import logo from "../../assets/images/logo.png";
+import styles from "./NavBar.module.css";
+
+const MobileNavBar = (props) => {
+  const { isLoggedIn, onLogout } = props;
+  const location = useLocation(); // Get the current location
+  const isActive = (path) => (location.pathname === path ? styles.active : "");
+  return (
+    <Navbar className={styles.background} expand="md">
+      <Container>
+        {/* <Nav className="me-auto col-md-5 justify-content-start justify-content-around">
+          <Nav.Link
+            className={`${styles.endText} ${isActive("/")}`}
+            as={Link}
+            to="/"
+          >
+            Home
+          </Nav.Link>
+
+          <Nav.Link
+            className={`${styles.navLink} ${isActive("/calendar")}`}
+            as={Link}
+            to="/calendar"
+          >
+            Calendar
+          </Nav.Link>
+          <Nav.Link
+            className={`${styles.navLink} ${isActive("/about-us")}`}
+            as={Link}
+            to="/about-us"
+          >
+            About Us
+          </Nav.Link>
+        </Nav> */}
+        <Navbar.Brand
+          as={Link}
+          to="/"
+          className="mx-auto d-flex justify-content-center col-md-2"
+        >
+          <img
+            src={logo}
+            width="48"
+            height="48"
+            alt="logo"
+            className="align-self-center rounded-4"
+          />
+        </Navbar.Brand>
+        <Nav className="ms-auto col-md-5 justify-content-end justify-content-around">
+          <Nav.Link
+            className={`${styles.navLink} ${isActive("/contact-us")}`}
+            as={Link}
+            to="/contact-us"
+          >
+            Contact Us
+          </Nav.Link>
+
+          {isLoggedIn ? (
+            <Nav.Link className={styles.endText} onClick={onLogout}>
+              Logout
+            </Nav.Link>
+          ) : (
+            <>
+              <Nav.Link
+                className={`${styles.navLink} ${isActive("/login")}`}
+                as={Link}
+                to="/login"
+              >
+                Login
+              </Nav.Link>
+              <Nav.Link
+                className={`${styles.endText} ${isActive("/register")}`}
+                as={Link}
+                to="/register"
+              >
+                Register
+              </Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+MobileNavBar.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  onLogout: PropTypes.func.isRequired,
+};
+
+export default MobileNavBar;
---
+++ react/src/layouts/MobileLayout.jsx
@@ -0,0 +1,25 @@
+import MobileNavBar from "../components/navBar/MobileNavBar";
+import Footer from "../components/footer/HomeFooter";
+import PropTypes from "prop-types";
+import { Container } from "react-bootstrap";
+import styles from "./Layout.module.css";
+
+const MobileLayout = (props) => {
+  return (
+    <>
+      <MobileNavBar {...props} />
+      <Container className={styles.defaultContainer}>
+        {props.children}
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+MobileLayout.propTypes = {
+  children: PropTypes.element,
+  isLoggedIn: PropTypes.bool.isRequired,
+  onLogout: PropTypes.func.isRequired,
+};
+
+export default MobileLayout;
