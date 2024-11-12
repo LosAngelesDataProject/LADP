c312fb323f1a79da09d25788efc2e017f278e0f8
Jesse James Canell Jr <119881707+jjcanell77@users.noreply.github.com>
2024-11-12 17:01:10 -0600

    Adjusted Top Navbar for Mobile  view

--- react/src/components/navBar/MobileNavBar.jsx
+++ react/src/components/navBar/MobileNavBar.jsx
@@ -1,40 +1,22 @@
-import { useState } from "react";
-import {
-  Link,
-  //  , useLocation
-} from "react-router-dom";
-import { Navbar, Container, Col } from "react-bootstrap";
+// import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import PropTypes from "prop-types";
 import logo from "../../assets/images/logo.png";
 import styles from "./NavBar.module.css";
+// import { useMediaQuery } from "react-responsive";
 
-const MobileNavBar = () => {
-  // const location = useLocation(); // Get the current location
-  // const isActive = (path) => (location.pathname === path ? styles.active : "");
-  // const SearchBar = () => {
-  //   return (
-  //     <>
-  //       <input
-  //         id="searchFilter"
-  //         type="search"
-  //         className={styles.searchInputLeft}
-  //         placeholder="Search"
-  //       />
-  //       <input
-  //         id="searchLocation"
-  //         type="search"
-  //         className={styles.searchInputRight}
-  //         placeholder="Location"
-  //       />
-  //       {/* <i
-  //         className={`fa-solid fa-magnifying-glass ${styles.searchIcon}`}
-  //         onClick={() => {}}
-  //       /> */}
-  //     </>
-  //   );
-  // };
-  const [toggleInput, setToggleInput] = useState({ input: false, type: "" });
+const MobileNavBar = (props) => {
+  const { isLoggedIn } = props;
+  // const isSmallPhone = useMediaQuery({
+  //   query: "(max-width: 370px)",
+  // });
+  const location = useLocation(); // Get the current location
+  const isActive = (path) => (location.pathname === path ? styles.active : "");
+  // const [toggleInput, setToggleInput] = useState("");
+
   return (
-    <Navbar className={styles.background} expand={false}>
+    <Navbar className={styles.background} expand={"sm"}>
       <Container>
         <Navbar.Brand as={Link} to="/" className={`${styles.navBrandText}`}>
           <img
@@ -46,62 +28,69 @@ const MobileNavBar = () => {
           />
           LADP
         </Navbar.Brand>
-        {/* <div> */}
-        {toggleInput.input ? (
-          <Col className={` align-middle`}>
+        <Nav>
+          <Nav.Link as={Link} to={isLoggedIn ? "/user" : "/login"}>
             <i
-              className={`fa-solid fa-arrow-left ${styles.searchIconLeft}`}
-              onClick={() => {
-                setToggleInput({ input: false, type: "" });
-              }}
+              className={`fa-solid fa-user ${styles.filterIcon} ${isActive(
+                "/login" || "/register" || "/user"
+              )}`}
             />
-            <input
-              id="searchFilter"
-              type="search"
-              className={styles.searchInput}
-              placeholder={toggleInput.type}
+          </Nav.Link>
+        </Nav>
+        {/* <div className={`${isSmallPhone ? `w-100 pt-1` : "col"}`}>
+          <i
+            className={`fa-solid fa-arrow-left ${styles.searchIconLeft} ${
+              toggleInput === "" ? "d-none" : ""
+            }`}
+            onClick={() => {
+              setToggleInput("");
+            }}
+          />
+          <input
+            id="searchFilter"
+            type="text"
+            placeholder="&#xF002; Search"
+            className={`${styles.searchInputLeft} ${
+              toggleInput === "search" ? styles.expand : "w-50"
+            } ${toggleInput === "location" ? "d-none" : ""}
+            `}
+            onClick={() => {
+              setToggleInput("search");
+            }}
+          />
+          <input
+            id="searchLocation"
+            type="text"
+            placeholder="&#xF3C5; Location"
+            className={`${styles.searchInputRight} ${
+              toggleInput === "location" ? styles.expand : "w-50"
+            }  ${toggleInput === "search" ? "d-none" : ""}`}
+            onSelect={() => {
+              setToggleInput("location");
+            }}
+          />
+          {toggleInput === "search" ? (
+            <i
+              className={`fa-solid fa-magnifying-glass ${styles.searchIconRight}`}
+              onClick={() => {}}
             />
-            {toggleInput.type === "Search" ? (
-              <i
-                className={`fa-solid fa-magnifying-glass ${styles.searchIconRight}`}
-                onClick={() => {}}
-              />
-            ) : (
-              <i
-                className={`fa-solid fa-location-dot ${styles.searchIconRight}`}
-                onClick={() => {}}
-              />
-            )}
-          </Col>
-        ) : (
-          <Col>
-            <button
-              className={styles.searchButton}
-              onClick={() => {
-                setToggleInput({ input: true, type: "Search" });
-              }}
-            >
-              <i
-                className={`fa-solid fa-magnifying-glass ${styles.buttonIcon}`}
-              />
-              Search
-            </button>
-            <button
-              className={styles.locationButton}
-              onClick={() => {
-                setToggleInput({ input: true, type: "Location" });
-              }}
-            >
-              <i className={`fa-solid fa-location-dot ${styles.buttonIcon}`} />
-              Location
-            </button>
-          </Col>
-        )}
-
-        {/* </div> */}
+          ) : (
+            <i
+              className={`fa-solid fa-location-dot ${styles.searchIconRight} ${
+                toggleInput === "" ? "d-none" : ""
+              }`}
+              onClick={() => {}}
+            />
+          )}
+        </div> */}
       </Container>
     </Navbar>
   );
+};
+
+MobileNavBar.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  onLogout: PropTypes.func.isRequired,
 };
 
 export default MobileNavBar;
