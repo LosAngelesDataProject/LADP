7b1334d86f0e8a3c18e8834d5e15d0049af27391
Jesse James Canell Jr <119881707+jjcanell77@users.noreply.github.com>
2024-11-22 13:57:21 -0600

    fixed duplicate import in App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,7 +4,6 @@ import "./App.css";
 import AboutUs from "./components/about/AboutUs";
 import Calendar from "./components/calender/Calendar";
 import Construction from "./components/underConstruction/Construction";
-import Construction from "./components/underConstruction/Construction";
 import Home from "./components/home/Home";
 // import Login from "./components/login/Login";
 import Navigate from "./components/Navigate";
