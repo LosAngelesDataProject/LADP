b5c1c4e3490765536ab1c31d5018a147d06d4f9f
Jared <117613416+na106@users.noreply.github.com>
2025-08-25 17:27:57 -0700

    Update RepositoryEmail.cs
    Removed hard-coded parts

--- dotnet/LADP_EFC/Repository/RepositoryEmail.cs
+++ dotnet/LADP_EFC/Repository/RepositoryEmail.cs
@@ -38,7 +38,7 @@ namespace LADP_EFC.Repository
                     },
                     To = new List<SendSmtpEmailTo>
                     {
-                        new SendSmtpEmailTo("na106@mail.chapman.edu")
+                        new SendSmtpEmailTo("Your-Email-Address")
                     },
                     Subject = "Test Email",
                     HtmlContent = "<html><body><h1>This is a test email</h1></body></html>"
