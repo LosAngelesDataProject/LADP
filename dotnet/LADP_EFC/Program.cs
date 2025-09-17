eb3e4863e2ccca490a24eb7b9efdb0db43cce203
Jared <117613416+na106@users.noreply.github.com>
2025-09-16 18:58:14 -0700

    Update Program.cs
    Removed commented out code in program.cs

--- dotnet/LADP_EFC/Program.cs
+++ dotnet/LADP_EFC/Program.cs
@@ -36,18 +36,11 @@ namespace LADP__EFC
                         .AllowAnyHeader()
                         .AllowCredentials());
             });
-
-
+            
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            /*
-            builder.Services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-            });
-            */
+            
             builder.Services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlServer(
