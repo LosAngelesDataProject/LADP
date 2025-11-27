04efcebcfeb45aab6777c5c4ace99fbe7761a53e
Jared <117613416+na106@users.noreply.github.com>
2025-11-26 17:32:11 -0800

    Update UnitTest1.cs
    Removed duplicate codes from merge editor

--- dotnet/LADP.Tests/UnitTest1.cs
+++ dotnet/LADP.Tests/UnitTest1.cs
@@ -1,15 +1,5 @@
 namespace LADP.Tests
 {
-<<<<<<< HEAD
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-
-    }
-}
-=======
     public class UnitTest1
     {
         [Fact]
@@ -18,5 +8,4 @@ public class UnitTest1
 
         }
     }
->>>>>>> 606e316 (Installed NuGet packages and file for createUser unit testing)
 }
